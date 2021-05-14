39945dfc0ccc8257cbcf2a1bba0c0f9d17946080
xrk <earth@mail.ustc.edu.cn>
2021-05-14 16:05:33 +0800

    speed

---
+++ libwen/speed/TrackWorm.m
@@ -0,0 +1,39 @@
+XX=1;
+YY=2;
+N=iend-istart+1;
+Centerline = zeros(N,100,2);
+time_speed = zeros(iend-istart+1,1);
+for i = 1:N
+    j = istart+i-1;
+    centerline=reshape(mcd(j).SegmentedCenterline,2,[]);
+    Centerline(i,:,1)=centerline(1,:);%x axis
+    Centerline(i,:,2)=centerline(2,:);%y axis
+    time_speed(i) = mcd(j).TimeElapsed;
+end
+
+
+%% calculte worm postion
+%CalculationMethodSelection = 1; %2 means using centroid, 1 means using multiple body points (segmented).
+switch CalculationMethodSelection
+    case 1 % using multiple points
+        segments = 20; %num of body points
+        start_counting_segment = 7; % n th point to start calculation
+        headpoint = 1; % position intra-segment 
+        com = zeros(N,2,segments-start_counting_segment+1);
+        for segment = start_counting_segment:segments
+        com(:,:,segment-segment+1) = [Centerline(:,headpoint+(segment-1)*100/segments,XX),...
+            Centerline(:,headpoint+(segment-1)*100/segments,YY)];
+        end
+        
+
+    case 2 % using centroid
+        segments = 1;
+        start_counting_segment = 1;
+        ignoring_segments_head = 30;      
+        com=[mean(Centerline(:,ignoring_segments_head:end,XX),2), mean(Centerline(:,ignoring_segments_head:end,YY),2)]; 
+        
+    case 3
+        segments=1;
+        start_counting_segment=1;
+        com=zeros(N,2,segments);
+end
---
+++ speed.m
@@ -0,0 +1,11 @@
+function y=speed(A)
+%算质心速度speed随时间变化的数据
+micronperunit = 0.05;  %每个单位位移代表的长度 mm
+micronperpxl = 2.43;  %每个像素代表的长度 mm
+time=A.TimeElapse
+centerPosion=centeroidPosition(A);   %质心相对相机的时间-位移 X1
+
+
+x1=mcd.
+ 
+end
