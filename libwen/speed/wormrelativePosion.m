89c02c6f02e793c1c1a16793c3de32cd229b3d99
xrk <earth@mail.ustc.edu.cn>
2021-05-15 20:14:58 +0800

    speed

--- libwen/speed/wormrelativePosion.m
+++ libwen/speed/wormrelativePosion.m
@@ -1,18 +1,20 @@
 function relativePosion=wormrelativePosion(A)
 %算质心速度speed随时间变化的数据
+%A is wormdata
 micronperunit = 0.05;  %每个单位位移代表的长度 mm
 micronperpxl = 2.43;  %每个像素代表的长度 mm
 time=A.TimeElapsed;
-centerPosion=centeroidPosition(A);   %质心相对相机的时间-位移 X1
-
 StagePosition=A.StagePosition;
+centerPosition=centeroidPosion(A);   %质心相对相机的时间-位移 X1
+
+StagePosition=A.StagePosition;%wormdata.StagePosion
 snumfram=length(time);
 
 relativePosion=zeros(snumfram,3);
 
-for i=1:snumram
+for i=1:snumfram
    relativePosion(i,1)=time(i);
-   relativePosion(i,2)=micronperpxl*centerPosion(i,2)-micronperunit*StagePosition(i,2);
-   relativePosion(i,3)=micronperpxl*centerPosion(i,3)-micronperunit*StagePosition(i,3);
+   relativePosion(i,2)=micronperpxl*centerPosition(i,2)-micronperunit*StagePosition(i,1);
+   relativePosion(i,3)=micronperpxl*centerPosition(i,3)-micronperunit*StagePosition(i,2);
 end
 end
